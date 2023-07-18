f65a2ed767c4c73f3828f799361c4c8f75519ae6
BernardoFatela <bernardo@fatela.com>
2023-07-18 23:35:19 +0100

    Refactoring done to be simpler to understand

--- tests/test_search_home.py
+++ tests/test_search_home.py
@@ -7,39 +7,15 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.keys import Keys
 import pytest
 import time
+from helper_functions import input_text,click_element
 
 
 def test_search_home(setup,driver):
-    wait = WebDriverWait(driver,10)
-    try:
-        address_input = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//input[@id='search-field']"))
-        )
-        address_input.send_keys("Austin, Texas")
-        address_input.send_keys(Keys.RETURN)
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("'Austin / Central Texas, TX area' link not found or not clickable.")
-    try:
-        link = wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//a[@data-testid='search-bar-result-cities-regions-0']"))
-        )
-        link.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("'Austin / Central Texas, TX area' link not found or not clickable.")
-    
-    try:
-        button = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@data-testid='search-results-filter-availability']"))
-        )
-        button.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("'Availability' button not found or not clickable.")
-#dropdown is clicked and now we select element
-
-    try:
-        checkbox = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@class='AvailabilityCheckbox_inputWrapper__FSw5R']"))
-        )
-        checkbox.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("'QuickMoveInHomes' checkbox not found or not clickable.")
+    input_text(driver, "//input[@id='search-field']", "Austin, Texas", "'Austin / Central Texas, TX area' link not found or not clickable.")
+    time.sleep(3)
+    click_element(driver, "//a[@data-testid='search-bar-result-cities-regions-0']", "'Austin / Central Texas, TX area' link not found or not clickable.")
+    time.sleep(3)
+    click_element(driver, "//button[@data-testid='search-results-filter-availability']", "'Availability' button not found or not clickable.")
+    time.sleep(3)
+    click_element(driver, "//div[@class='AvailabilityCheckbox_inputWrapper__FSw5R']", "'QuickMoveInHomes' checkbox not found or not clickable.")
+    time.sleep(3)
--- tests/test_sign_out.py
+++ tests/test_sign_out.py
@@ -6,61 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import pytest
 import time
+from helper_functions import login,click_element_when_visible
 
 #signs_in and then out
 def test_sign_out(setup,driver):
-    wait = WebDriverWait(driver, 10)
+    login(driver, "test@gmail.com", "test1234!")
     # Click on the button to open the panel
-    try:
-        open_panel_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='open-panel']"))
-        )
-        open_panel_button.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("Open panel button not found or not clickable.")
-
-    # Wait until the "Sign in or create" link is visible
-    try:
-        sign_in_link = wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//a[@class='AuthMenuContent_authLink__lP2HC']"))
-        )
-        sign_in_link.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("'Sign in or create' link not found or not clickable.")
-
-    #new page email_input
-    try:    
-        email_input = wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//input[@type='email']"))
-        )
-        email_input.send_keys("test@gmail.com")
-        continue_button = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'continue-button')]"))
-        )
-        continue_button.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("Error related to email insertion")
-
-    #new page password_input
-    try:    
-        password_input = wait.until(
-            EC.visibility_of_element_located((By.XPATH, "//input[@type='password']"))
-        )
-        password_input.send_keys("test1234!")
-        continue_button = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'continue-button')]"))
-        )
-        continue_button.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("Could not sign in after inputing password.")
-
-    #end of sign in
-    # Click on the button to open the panel
-    try:
-        log_out_button = wait.until(
-        EC.visibility_of_element_located((By.XPATH, "//button[@aria-label='Log out']"))
-        )
-        log_out_button.click()
-    except (NoSuchElementException, TimeoutException):
-        pytest.fail("'Sign in or create' link not found or not clickable.")
+    click_element_when_visible(driver, "//button[@aria-label='Log out']", "'Log out' link not found or not clickable.")
     
