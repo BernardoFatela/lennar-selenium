09adef5955d2c54655a0f364618778a155866fdd
BernardoFatela <bernardo@fatela.com>
2023-07-18 20:05:36 +0100

    Adding initial files for tests

---
+++ tests/test_sign_up.py
@@ -0,0 +1,11 @@
+# test_sign_up.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import pytest
+
+
+def test_sign_up(setup):
+    i = 0
